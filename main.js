2ea1770bde1c0dd0c2a6330a557d32646d5f2da6
kuba <143841110+hakaridc@users.noreply.github.com>
2025-05-08 12:28:53 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function adicionarTarefa(){
+    const imput = document.getElementByid('nova-tarefa');
+    const textoTarefa = input.value.trim();
+}
+
+if (textpTarefa === "") return;
+
+-----paramos na linha 71------
